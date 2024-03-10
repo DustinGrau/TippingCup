0376cd742026c6041eab6fec5d300329cde94bc9
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-03-10 11:13:12 -0400

    Update TippyCup.ino

--- src/TippyCup/TippyCup.ino
+++ src/TippyCup/TippyCup.ino
@@ -8,7 +8,8 @@ const int analogPinA1 = A1; // A1 pin, maps to the DOWN or RIGHT button on remot
 const int analogPinA2 = A2; // A2 pin, maps to the UP or LEFT button on remote
 const int analogPinA5 = A5; // A5 pin, maps to the TOGGLE button on remote
 const int servoPin = 9;     // Digital pin for servo control (D9)
-int servoPos = 0;           // Last-known servo position
+uint16_t servoPos = 0;      // Last-known servo position
+boolean b_tipped = false;   // Indicates whether the servo was just in the "tipped" state.
 
 Servo mainServo; // Create a servo object to control a servo motor
 
@@ -31,32 +32,50 @@ void loop() {
   // Check if A5 is HIGH, meaning the device
   // should be doing a wobble action
   if (sensorValueA5 > 600) {
-    if (servoPos > 0) {
-      // Always reset if anything other than
-      // the default (upright) position.
-      servoPos = 0;
-      mainServo.write(servoPos);
-      delay(random(100,300)); // Delay before next action
+    // Always reset if anything other than
+    // the default (upright) position.
+    if(resetPosition()) {
+      delay(random(100,400)); // Delay before next action
     }
 
-    servoPos = random(40,80);
+    servoPos = random(20,60);
     mainServo.write(servoPos);
-    delay(random(200,400)); // Delay before next action
+    delay(random(200,500)); // Delay before next action
   } else {
     // Check if A1 is HIGH
     if (sensorValueA1 > 600) {
-      // Return the servo to 0 degrees (upright)
-      servoPos = 0;
-      mainServo.write(servoPos);
-      delay(500); // Delay before next input
+      resetPosition();
+      delay(500); // Delay before next action
+      b_tipped = false; // Reset for next tipping event
     }
 
     // Check if A2 is HIGH
-    if (sensorValueA2 > 600) {
+    if (sensorValueA2 > 600 && !b_tipped) {
       // Turn the servo 180 degrees (tipped)
       servoPos = 180;
       mainServo.write(servoPos);
-      delay(500); // Delay before next input
+      delay(250); // Delay before next action
+
+      // Return to a reset position
+      resetPosition();
+      delay(500); // Delay before next action
+
+      // Set latch to ensure sensor state only
+      // causes a single servo/tipping event
+      b_tipped = true;
     }
   }
+}
+
+boolean resetPosition() {
+  boolean b_changed = false;
+
+  // Return the servo to 0 degrees (upright)
+  if(servoPos > 0) {
+    servoPos = 0;
+    b_changed = true;
+    mainServo.write(servoPos);
+  }
+
+  return b_changed;
 }
